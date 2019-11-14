dc4f80f8333d3ef85b09b9a4784ea2742cddfaf6
Orion91 <flinch217@gmail.com>
2019-11-14 20:46:52 +0100

    Reorganizing projects files

--- src/AuthService.Application/Commands/SignUp.cs
+++ src/AuthService.Application/Commands/SignUp.cs
@@ -4,13 +4,8 @@ namespace AuthService.Application.Commands
 {
     public class SignUp : ICommand
     {
-        [Required]
         public string Username { get; set; }
-        [Required]
-        [StringLength(16, MinimumLength = 6, ErrorMessage = "You must specify password between 6 and 16 characters")]
         public string Password { get; set; }
-        [EmailAddress]
         public string Email { get; set; }
-
     }
 }
