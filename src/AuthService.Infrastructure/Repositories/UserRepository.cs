9a0668867cc95c173cd8a740a6d55f9a3d73fda1
Orion91 <flinch217@gmail.com>
2019-11-24 18:36:05 +0100

    Added implementation for creating new user in sql database

--- src/AuthService.Infrastructure/Repositories/UserRepository.cs
+++ src/AuthService.Infrastructure/Repositories/UserRepository.cs
@@ -1,8 +1,11 @@
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Threading.Tasks;
 using AuthService.Core.Models;
 using AuthService.Core.Repositories;
 using AuthService.Infrastructure.Settings;
+using Dapper;
 
 namespace AuthService.Infrastructure.Repositories
 {
@@ -14,9 +17,22 @@ namespace AuthService.Infrastructure.Repositories
 		{
 			_sqlSettings = sqlSettings;
 		}
-        public Task AddAsync(User user)
+        public async Task AddAsync(User user)
         {
-            throw new System.NotImplementedException();
+			using (IDbConnection conn = new SqlConnection(_sqlSettings.ConnectionString))
+			{
+				if (conn.State == ConnectionState.Closed)
+					conn.Open();
+
+				DynamicParameters parameters = new DynamicParameters();
+				parameters.Add("@Username", user.Username);
+				parameters.Add("@Email", user.Email);
+				parameters.Add("@PasswordHash", user.PasswordHash);
+				parameters.Add("@PasswordSalt", user.PasswordSalt);
+				parameters.Add("@CreatedDate", user.CreatedAt);
+				parameters.Add("@LastActiveDate", user.LastActive);
+				await conn.ExecuteAsync("Users_CreateNewUser", parameters, commandType: CommandType.StoredProcedure);
+			}
         }
 
         public Task<User> GetAsync(string username)
