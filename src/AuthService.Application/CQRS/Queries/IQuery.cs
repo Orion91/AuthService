69d43f3dacd56d4dc8b980e0138f5455f56c450a
Orion91 <flinch217@gmail.com>
2019-11-29 21:31:50 +0100

    Changed location of Command interfaces and added interfaces for queries

---
+++ src/AuthService.Application/CQRS/Queries/IQuery.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuthService.Application.CQRS
+{
+	public interface IQuery<T>
+	{
+	}
+}
---
+++ src/AuthService.Application/CQRS/Queries/IQueryDispatcher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthService.Application.CQRS.Queries
+{
+	public interface IQueryDispatcher
+	{
+		Task DispatchAsync<TQuery, TResult>(TQuery query) where TQuery : IQuery<TResult>;
+	}
+}
